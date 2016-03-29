3d3b09dcd97041c52562925bdd9eed20b793db71
Alessio Sclocco <alessio@sclocco.eu>
2016-03-29 16:37:01 +0200

    MD can now exploit data reuse for the neighbors.

--- src/MD.cpp
+++ src/MD.cpp
@@ -23,10 +23,12 @@ std::string * getMDOpenCL(const isa::OpenCL::KernelConf & conf, const std::strin
   // Begin kernel's code
   *code = "__kernel void MD(__global const " + dataName + "4 * const restrict input, __global " + dataName + "4 * const restrict output) {\n"
     "<%DEFPOSITION%>"
+    "<%DEFNEIGHBOR%>"
     "<%DEF%>"
     "for ( const unsigned int neighbor = 0; neighbor < " + isa::utils::toString(nrAtoms) + "; neighbor += " + isa::utils::toString(conf.getNrItemsD1()) + " ) {\n"
     + dataName + " distance = 0.0;\n"
     + dataName + " force = 0.0;\n"
+    "<%LOADNEIGHBOR%>"
     "<%COMPUTE%>"
     "}\n"
     "// Optional in-thread reduction\n"
@@ -35,22 +37,22 @@ std::string * getMDOpenCL(const isa::OpenCL::KernelConf & conf, const std::strin
     "<%STORE%>"
     "}\n";
   std::string defPosition_sTemplate = dataName + "4 position<%NUMD0%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
-  std::string def_sTemplate = dataName + "4 neighbor<%NUMD0%>x<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n"
-    + dataName + "4 accumulator<%NUMD0%>x<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
-  std::string compute_sTemplate = "neighbor<%NUMD0%>x<%NUMD1%> = input[neighbor + <%OFFSETD1%>];\n"
-    "distance = 1.0f / (((position<%NUMD0%>.x - neighbor<%NUMD0%>x<%NUMD1%>.x) * (position<%NUMD0%>.x - neighbor<%NUMD0%>x<%NUMD1%>.x)) + ((position<%NUMD0%>.y - neighbor<%NUMD0%>x<%NUMD1%>.y) * (position<%NUMD0%>.y - neighbor<%NUMD0%>x<%NUMD1%>.y)) + ((position<%NUMD0%>.z - neighbor<%NUMD0%>x<%NUMD1%>.z) * (position<%NUMD0%>.z - neighbor<%NUMD0%>x<%NUMD1%>.z)));\n"
+  std::string defNeighbor_sTemplate = dataName + "4 neighbor<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
+  std::string def_sTemplate = dataName + "4 accumulator<%NUMD0%>x<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
+  std::string loadNeighbor_sTemplate = "neighbor<%NUMD1%> = input[neighbor + <%OFFSETD1%>];\n";
+  std::string compute_sTemplate = "distance = 1.0f / (((position<%NUMD0%>.x - neighbor<%NUMD1%>.x) * (position<%NUMD0%>.x - neighbor<%NUMD1%>.x)) + ((position<%NUMD0%>.y - neighbor<%NUMD1%>.y) * (position<%NUMD0%>.y - neighbor<%NUMD1%>.y)) + ((position<%NUMD0%>.z - neighbor<%NUMD1%>.z) * (position<%NUMD0%>.z - neighbor<%NUMD1%>.z)));\n"
     "force = (distance * distance * distance * distance) * ((" + isa::utils::toString(LJ1) + "f * distance * distance * distance) - " + isa::utils::toString(LJ2) + "f);\n"
-    "if ( get_global_id(0) != neighbor ) {\n"
-    "accumulator<%NUMD0%>x<%NUMD1%>.x += (position<%NUMD0%>.x - neighbor<%NUMD0%>x<%NUMD1%>.x) * force;\n"
-    "accumulator<%NUMD0%>x<%NUMD1%>.y += (position<%NUMD0%>.y - neighbor<%NUMD0%>x<%NUMD1%>.y) * force;\n"
-    "accumulator<%NUMD0%>x<%NUMD1%>.z += (position<%NUMD0%>.z - neighbor<%NUMD0%>x<%NUMD1%>.z) * force;\n"
-    "}\n";
+    "accumulator<%NUMD0%>x<%NUMD1%>.x += (position<%NUMD0%>.x - neighbor<%NUMD1%>.x) * force;\n"
+    "accumulator<%NUMD0%>x<%NUMD1%>.y += (position<%NUMD0%>.y - neighbor<%NUMD1%>.y) * force;\n"
+    "accumulator<%NUMD0%>x<%NUMD1%>.z += (position<%NUMD0%>.z - neighbor<%NUMD1%>.z) * force;\n";
   std::string reduce_sTemplate = "accumulator<%NUMD0%>x0 += accumulator<%NUMD0%>x<%NUMD1%>;\n";
   std::string store_sTemplate = "output[get_global_id(0) + <%OFFSETD0%>] = accumulator<%NUMD0%>x0;\n";
   // End kernel's code
 
   std::string * defPosition_s = new std::string();
+  std::string * defNeighbor_s = new std::string();
   std::string * def_s = new std::string();
+  std::string * loadNeighbor_s = new std::string();
   std::string * compute_s = new std::string();
   std::string * reduce_s = new std::string();
   std::string * store_s = new std::string();
@@ -104,14 +106,35 @@ std::string * getMDOpenCL(const isa::OpenCL::KernelConf & conf, const std::strin
     store_s->append(*temp);
     delete temp;
   }
+  for ( unsigned int d1 = 0; d1 < conf.getNrItemsD1(); d1++ ) {
+    std::string d1_s = isa::utils::toString(d1);
+    std::string offsetD1_s = isa::utils::toString(d1 * conf.getNrItemsD1());
+    std::string * temp = 0;
+
+    temp = isa::utils::replace(&defNeighbor_sTemplate, "<%NUMD1%>", d1_s);
+    defNeighbor_s->append(*temp);
+    delete temp;
+    temp = isa::utils::replace(&loadNeighbor_sTemplate, "<%NUMD1%>", d1_s);
+    if ( d1 == 0 ) {
+      temp = isa::utils::replace(temp, " + <%OFFSETD1%>", empty_s, true);
+    } else {
+      temp = isa::utils::replace(temp, "<%OFFSETD1%>", offsetD1_s, true);
+    }
+    loadNeighbor_s->append(*temp);
+    delete temp;
+  }
 
   code = isa::utils::replace(code, "<%DEFPOSITION%>", *defPosition_s, true);
+  code = isa::utils::replace(code, "<%DEFNEIGHBOR%>", *defNeighbor_s, true);
   code = isa::utils::replace(code, "<%DEF%>", *def_s, true);
+  code = isa::utils::replace(code, "<%LOADNEIGHBOR%>", *loadNeighbor_s, true);
   code = isa::utils::replace(code, "<%COMPUTE%>", *compute_s, true);
   code = isa::utils::replace(code, "<%REDUCE%>", *reduce_s, true);
   code = isa::utils::replace(code, "<%STORE%>", *store_s, true);
   delete defPosition_s;
+  delete defNeighbor_s;
   delete def_s;
+  delete loadNeighbor_s;
   delete compute_s;
   delete reduce_s;
   delete store_s;
