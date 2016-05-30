795dd8cf819f00163033bdd2ba84abe0cbfd0b71
Alessio Sclocco <alessio@sclocco.eu>
2016-05-30 11:34:41 +0200

    New organization for the code. More interleaving between instructions.

--- src/MD.cpp
+++ src/MD.cpp
@@ -40,8 +40,6 @@ std::string * getMDOpenCL(const isa::OpenCL::KernelConf & conf, const std::strin
     "\n"
     "<%LOAD_POSITION%>"
     "for ( unsigned int neighbor = 0; neighbor < " + isa::utils::toString(nrAtoms) + "; neighbor += " + isa::utils::toString(conf.getNrItemsD1()) + " ) {\n"
-    + dataName + " inverseDistance = 0.0;\n"
-    + dataName + " force = 0.0;\n"
     "<%LOADNEIGHBOR%>"
     "<%COMPUTE%>"
     "}\n"
@@ -52,14 +50,17 @@ std::string * getMDOpenCL(const isa::OpenCL::KernelConf & conf, const std::strin
     "}\n";
   std::string defPosition_sTemplate = dataName + "4 position<%NUMD0%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
   std::string defNeighbor_sTemplate = dataName + "4 neighbor<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
-  std::string def_sTemplate = dataName + "4 accumulator<%NUMD0%>x<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n";
+  std::string def_sTemplate = dataName + "4 accumulator<%NUMD0%>x<%NUMD1%> = {0.0f, 0.0f, 0.0f, 0.0f};\n"
+    + dataName + " inverseDistance<%NUMD0%>x<%NUMD1%> = 0.0;\n"
+    + dataName + " force<%NUMD0%>x<%NUMD1%> = 0.0;\n";
   std::string loadPosition_sTemplate = "position<%NUMD0%> = input[(get_group_id(0) * " + std::to_string(conf.getNrThreadsD0() * conf.getNrItemsD0()) + ") + get_local_id(0) + <%OFFSETD0%>];\n";
   std::string loadNeighbor_sTemplate = "neighbor<%NUMD1%> = input[neighbor + <%OFFSETD1%>];\n";
-  std::string compute_sTemplate = "inverseDistance = 1.0f / (((position<%NUMD0%>.x - neighbor<%NUMD1%>.x) * (position<%NUMD0%>.x - neighbor<%NUMD1%>.x)) + ((position<%NUMD0%>.y - neighbor<%NUMD1%>.y) * (position<%NUMD0%>.y - neighbor<%NUMD1%>.y)) + ((position<%NUMD0%>.z - neighbor<%NUMD1%>.z) * (position<%NUMD0%>.z - neighbor<%NUMD1%>.z)));\n"
-    "force = (inverseDistance * inverseDistance * inverseDistance * inverseDistance) * ((" + LJ1_s + " * (inverseDistance * inverseDistance * inverseDistance)) - " + LJ2_s + ");\n"
-    "accumulator<%NUMD0%>x<%NUMD1%>.x += (position<%NUMD0%>.x - neighbor<%NUMD1%>.x) * force;\n"
-    "accumulator<%NUMD0%>x<%NUMD1%>.y += (position<%NUMD0%>.y - neighbor<%NUMD1%>.y) * force;\n"
-    "accumulator<%NUMD0%>x<%NUMD1%>.z += (position<%NUMD0%>.z - neighbor<%NUMD1%>.z) * force;\n";
+  std::vector< std::string > compute_sTemplate(5);
+  compute_sTemplate[0] = "inverseDistance<%NUMD0%>x<%NUMD1%> = 1.0f / (((position<%NUMD0%>.x - neighbor<%NUMD1%>.x) * (position<%NUMD0%>.x - neighbor<%NUMD1%>.x)) + ((position<%NUMD0%>.y - neighbor<%NUMD1%>.y) * (position<%NUMD0%>.y - neighbor<%NUMD1%>.y)) + ((position<%NUMD0%>.z - neighbor<%NUMD1%>.z) * (position<%NUMD0%>.z - neighbor<%NUMD1%>.z)));\n";
+  compute_sTemplate[1] =  "force<%NUMD0%>x<%NUMD1%> = (inverseDistance<%NUMD0%>x<%NUMD1%> * inverseDistance<%NUMD0%>x<%NUMD1%> * inverseDistance<%NUMD0%>x<%NUMD1%> * inverseDistance<%NUMD0%>x<%NUMD1%>) * ((" + LJ1_s + " * (inverseDistance<%NUMD0%>x<%NUMD1%> * inverseDistance<%NUMD0%>x<%NUMD1%> * inverseDistance<%NUMD0%>x<%NUMD1%>)) - " + LJ2_s + ");\n";
+  compute_sTemplate[2] = "accumulator<%NUMD0%>x<%NUMD1%>.x += (position<%NUMD0%>.x - neighbor<%NUMD1%>.x) * force<%NUMD0%>x<%NUMD1%>;\n";
+  compute_sTemplate[3] = "accumulator<%NUMD0%>x<%NUMD1%>.y += (position<%NUMD0%>.y - neighbor<%NUMD1%>.y) * force<%NUMD0%>x<%NUMD1%>;\n";
+  compute_sTemplate[4] = "accumulator<%NUMD0%>x<%NUMD1%>.z += (position<%NUMD0%>.z - neighbor<%NUMD1%>.z) * force<%NUMD0%>x<%NUMD1%>;\n";
   std::string reduce_sTemplate = "accumulator<%NUMD0%>x0 += accumulator<%NUMD0%>x<%NUMD1%>;\n";
   std::string store_sTemplate = "output[(get_group_id(0) * " + std::to_string(conf.getNrThreadsD0() * conf.getNrItemsD0()) + ") + get_local_id(0) + <%OFFSETD0%>] = accumulator<%NUMD0%>x0;\n";
   // End kernel's code
@@ -89,14 +90,16 @@ std::string * getMDOpenCL(const isa::OpenCL::KernelConf & conf, const std::strin
       temp = isa::utils::replace(&def_sTemplate, "<%NUMD1%>", d1_s);
       def_s->append(*temp);
       delete temp;
-      temp = isa::utils::replace(&compute_sTemplate, "<%NUMD1%>", d1_s);
-      if ( d1 == 0 ) {
-        temp = isa::utils::replace(temp, " + <%OFFSETD1%>", empty_s, true);
-      } else {
-        temp = isa::utils::replace(temp, "<%OFFSETD1%>", offsetD1_s, true);
+      for ( unsigned int computeStatement = 0; computeStatement < 5; computeStatement++ ) {
+        temp = isa::utils::replace(&compute_sTemplate[computeStatement], "<%NUMD1%>", d1_s);
+        if ( d1 == 0 ) {
+          temp = isa::utils::replace(temp, " + <%OFFSETD1%>", empty_s, true);
+        } else {
+          temp = isa::utils::replace(temp, "<%OFFSETD1%>", offsetD1_s, true);
+        }
+        compute_s->append(*temp);
+        delete temp;
       }
-      compute_s->append(*temp);
-      delete temp;
       if ( d1 > 0 ) {
         temp = isa::utils::replace(&reduce_sTemplate, "<%NUMD1%>", d1_s);
         reduce_s->append(*temp);
